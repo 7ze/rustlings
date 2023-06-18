8596051f988504abb4a6457d87f7c73abb82aeed
The_philosopher <email.shinudonney@gmail.com>
2023-06-18 08:56:29 +0200

    WIP - chapter 4

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: u32) -> u32 {
+    let apple_price = if apple_count > 40 { 1 } else { 2 };
+    apple_count * apple_price
+}
 
 // Don't modify this function!
 #[test]
